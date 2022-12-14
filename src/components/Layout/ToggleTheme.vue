3ae5ddeceb6f7ea199e4c2f2a616d8519734994b
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2022-12-14 10:40:10 +0100

    feat: implement ToggleTheme component

---
+++ src/components/Layout/ToggleTheme.vue
@@ -0,0 +1,38 @@
+<template>
+    <n-tooltip placement="bottom" :disabled="disableTooltip">
+        <template #trigger>
+            <div
+                class="h-8 w-8 grid place-items-center rounded-lg cursor-pointer bg-gray-200/25 hover:bg-gray-300/25 dark:(bg-gray-700/25 hover:bg-gray-600/25 overflow-hidden)"
+                :class="buttonClass"
+                @click="appStore.toggleTheme()"
+            >
+                <mdi-white-balance-sunny
+                    class="transition-all ease-in-out duration-150 transform"
+                    :class="appStore.isDark ? 'translate-y-[50%]' : 'translate-y-[-100px]'"
+                />
+                <ic-baseline-dark-mode
+                    class="transition-all ease-in-out duration-150 transform"
+                    :class="!appStore.isDark ? 'translate-y-[-50%]' : 'translate-y-[100px]'"
+                />
+            </div>
+        </template>
+        <span>{{ appStore.isDark ? "Light" : "Dark" }}</span>
+    </n-tooltip>
+</template>
+
+<script setup lang="ts">
+    import { useAppStore } from "~/store/appStore";
+
+    const appStore = useAppStore();
+
+    // eslint-disable-next-line no-undef
+    const props = defineProps({
+        buttonClass: {
+            type: String,
+        },
+        disableTooltip: {
+            type: Boolean,
+            default: false,
+        },
+    });
+</script>
